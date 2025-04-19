f856019ef961af265d97f3208dbeb2c429d751d2
SoulOriginal <57761401+SoulOriginal@users.noreply.github.com>
2025-04-20 01:27:29 +0200

    fix

--- modules/zinit/install.zsh
+++ modules/zinit/install.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-# Путь установки zinit
-ZINIT_HOME="${YAKUAKE_LIBS_PATH}/dist/zinit"
+# Путь установки zinit в соответствии с XDG Base Directory
+ZINIT_HOME="${YAKUAKE_LIBS_PATH}/dist/zinit/zinit.git"
 
 # Проверяем, установлен ли уже zinit
 if ! check_dir_exists "$ZINIT_HOME"; then
@@ -10,17 +10,29 @@ if ! check_dir_exists "$ZINIT_HOME"; then
     # Создаем директорию dist если она не существует
     ensure_dist_exists
     
-    # Создаем директорию для zinit
-    mkdir -p "$ZINIT_HOME"
+    # Создаем родительскую директорию для zinit
+    mkdir -p "$(dirname $ZINIT_HOME)"
     
     # Клонируем репозиторий
-    git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}/zinit.git"
+    git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
     
     # Удаляем .git директорию
-    rm -rf "${ZINIT_HOME}/zinit.git/.git"
+    rm -rf "${ZINIT_HOME}/.git"
     
     echo "zinit установлен успешно!"
 fi
 
 # Подключаем zinit
-source "${ZINIT_HOME}/zinit.git/zinit.zsh" 
+source "${ZINIT_HOME}/zinit.zsh"
+
+# Определяем базовые настройки zinit
+declare -A ZINIT
+ZINIT[BIN_DIR]="${ZINIT_HOME}"
+ZINIT[HOME_DIR]="${YAKUAKE_LIBS_PATH}/dist/zinit"
+ZINIT[PLUGINS_DIR]="${ZINIT[HOME_DIR]}/plugins"
+ZINIT[COMPLETIONS_DIR]="${ZINIT[HOME_DIR]}/completions"
+ZINIT[SNIPPETS_DIR]="${ZINIT[HOME_DIR]}/snippets"
+ZINIT[ZCOMPDUMP_PATH]="${ZINIT[HOME_DIR]}/.zcompdump"
+
+# Создаем необходимые директории
+mkdir -p "${ZINIT[PLUGINS_DIR]}" "${ZINIT[COMPLETIONS_DIR]}" "${ZINIT[SNIPPETS_DIR]}" 
