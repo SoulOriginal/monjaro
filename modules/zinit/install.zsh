f1cee52b3d2841081867b117e86426a243eb4125
SoulOriginal <57761401+SoulOriginal@users.noreply.github.com>
2025-04-20 01:28:59 +0200

    fix

--- modules/zinit/install.zsh
+++ modules/zinit/install.zsh
@@ -3,6 +3,15 @@
 # Путь установки zinit в соответствии с XDG Base Directory
 ZINIT_HOME="${YAKUAKE_LIBS_PATH}/dist/zinit/zinit.git"
 
+# Определяем базовые настройки zinit до установки
+declare -A ZINIT
+ZINIT[BIN_DIR]="${ZINIT_HOME}"
+ZINIT[HOME_DIR]="${YAKUAKE_LIBS_PATH}/dist/zinit"
+ZINIT[PLUGINS_DIR]="${ZINIT[HOME_DIR]}/plugins"
+ZINIT[COMPLETIONS_DIR]="${ZINIT[HOME_DIR]}/completions"
+ZINIT[SNIPPETS_DIR]="${ZINIT[HOME_DIR]}/snippets"
+ZINIT[ZCOMPDUMP_PATH]="${ZINIT[HOME_DIR]}/.zcompdump"
+
 # Проверяем, установлен ли уже zinit
 if ! check_dir_exists "$ZINIT_HOME"; then
     echo "Устанавливаем zinit..."
@@ -22,17 +31,15 @@ if ! check_dir_exists "$ZINIT_HOME"; then
     echo "zinit установлен успешно!"
 fi
 
+# Создаем необходимые директории
+mkdir -p "${ZINIT[PLUGINS_DIR]}" "${ZINIT[COMPLETIONS_DIR]}" "${ZINIT[SNIPPETS_DIR]}"
+
 # Подключаем zinit
 source "${ZINIT_HOME}/zinit.zsh"
 
-# Определяем базовые настройки zinit
-declare -A ZINIT
-ZINIT[BIN_DIR]="${ZINIT_HOME}"
-ZINIT[HOME_DIR]="${YAKUAKE_LIBS_PATH}/dist/zinit"
-ZINIT[PLUGINS_DIR]="${ZINIT[HOME_DIR]}/plugins"
-ZINIT[COMPLETIONS_DIR]="${ZINIT[HOME_DIR]}/completions"
-ZINIT[SNIPPETS_DIR]="${ZINIT[HOME_DIR]}/snippets"
-ZINIT[ZCOMPDUMP_PATH]="${ZINIT[HOME_DIR]}/.zcompdump"
-
-# Создаем необходимые директории
-mkdir -p "${ZINIT[PLUGINS_DIR]}" "${ZINIT[COMPLETIONS_DIR]}" "${ZINIT[SNIPPETS_DIR]}" 
+# Базовая инициализация
+autoload -Uz compinit
+compinit
+zinit light zdharma-continuum/zinit-annex-bin-gem-node
+zinit light zdharma-continuum/zinit-annex-patch-dl
+zinit light zdharma-continuum/zinit-annex-rust 
--- modules/zinit/main.zsh
+++ modules/zinit/main.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
-# Подключаем установку и конфигурацию zinit
+# Сначала подключаем установку и инициализацию
 source "${0:A:h}/install.zsh"
+
+# Автозагрузка функций
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# После инициализации подключаем конфигурацию
 source "${0:A:h}/config.zsh" 
